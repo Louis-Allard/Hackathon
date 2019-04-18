6e0d4495f83d4a338fc541e360042743612a3696
Nathan Marcy <nathan.marcy@live.fr>
2019-04-18 17:07:39 +0200

    Front call image from Eggs api

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import "Front.css";
 
 class Front extends React.Component{
     render(){
         return(
+            <div>
 
+<img src={this.props.src} style={{ width: '150px', height: '150px' }} />
+            </div>
         )
     }
 }
