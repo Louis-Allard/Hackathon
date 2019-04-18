a6eee5602fac3fa527a2041064b9574dfe724cbe
Johan Ceugnart <johan.ceugnart@gmail.com>
2019-04-18 19:12:03 +0200

    Edit header and footer and add mention legale on modal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Modal from './Modal';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Header</h1>
+        
+        <h1>Hackathon #1 Easter-egg chalenge</h1>
         </header>
 
         <footer className="Footer"> 
-          <p>footer</p>
+          <Modal />
+          <p>Hackathon réalisé par l'équpe JLAN !</p>
         </footer>
       </div>
     );
