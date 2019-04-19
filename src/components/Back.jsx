41c4ff21d6bf296d5b2ae811d2cd9ccd5a9ca88c
Nathan Marcy <nathan.marcy@live.fr>
2019-04-19 13:28:30 +0200

    power leveeeeeeeeeeeeeel

--- src/components/Back.jsx
+++ src/components/Back.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import "../cards.css";
 
 class Back extends React.Component{
+    
     render(){
+        const min = 1;
+        const max = 10;
+        let rand =  min + (Math.random() * (max-min)) ;
+
         return(
             <div>
-            <p className="energie"> energie</p>
+            <p className="energie">Power level : {Math.round(rand)} </p>
             <h1 className = "characterName"> {this.props.name} </h1>
             <img className = "charaImg" src={this.props.character} />
             <p className="description"><br/>{this.props.skills[0]}<br/>{this.props.skills[1]}<br/>{this.props.skills[2]} </p>
